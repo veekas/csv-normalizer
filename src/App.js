b852877969d4523319aa6f392e761e72be8e7799
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-01-03 18:13:07 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <footer className="App-footer">
+          <p className='App-thankyou'> Thank you for reviewing my application, Truss team!</p>
+          <a href="http://veekasmeansprogress.com/react-resume/resume.pdf" className="About-author"><h1>Veekas' Resume</h1></a>
+          </Link>
+        </footer>
+
+      </div>
+    );
+  }
+}
+
+export default App;
